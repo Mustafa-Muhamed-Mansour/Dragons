2db28dae36210dca37b00dce8cec6678052a7a01
Mustafa-Muhamed-Mansour <mostafamansour163@yahoo.com>
2025-04-30 18:40:58 +0300

    create image slider to "transformations" dragons

--- composeApp/src/commonMain/kotlin/com/dragons/app/compositions/states/StateData.kt
+++ composeApp/src/commonMain/kotlin/com/dragons/app/compositions/states/StateData.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.CardElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun StateData(
@@ -34,63 +33,8 @@ fun StateData(
     ) {
         Column(
             modifier = modifier,
-//            Modifier.fillMaxSize().padding(all = 8.dp),
-//                Arrangement.Center,
-//                Alignment.CenterHorizontally
         ) {
             content(vertical, horizontal)
-//            val painter = rememberImagePainter(url = it.image,
-//                filterQuality = FilterQuality.High,
-//                placeholderPainter = { painterResource(Res.drawable.loading_white) },
-//                errorPainter = { painterResource(Res.drawable.loading_white) })
-//            Image(
-//                modifier = Modifier
-//                    .size(width = 600.dp, height = 400.dp),
-//                painter = painter,
-//                contentDescription = "dragon image",
-//                contentScale = ContentScale.Crop
-//            )
-//
-//            Text(
-//                text = it.name,
-//                modifier = Modifier.fillMaxWidth(),
-//                color = Color.Black,
-//                fontSize = 17.sp,
-//                fontWeight = FontWeight.ExtraBold,
-//                textAlign = TextAlign.Center
-//            )
-//
-//            Row(
-//                modifier = Modifier.fillMaxWidth()
-//
-//            ) {
-//                Text(
-//                    text = "Ki:",
-//                    color = Color.Black,
-//                    fontSize = 15.sp,
-//                    fontWeight = FontWeight.Bold
-//                )
-//                Text(
-//                    text = " ${it.ki}",
-//                    color = Color.Black,
-//                    fontSize = 15.sp
-//                )
-//            }
-//            Row(
-//                modifier = Modifier.fillMaxWidth()
-//            ) {
-//                Text(
-//                    text = "Max Ki:",
-//                    color = Color.Black,
-//                    fontSize = 15.sp,
-//                    fontWeight = FontWeight.Bold
-//                )
-//                Text(
-//                    text = " ${it.maxKi}",
-//                    color = Color.Black,
-//                    fontSize = 15.sp
-//                )
-//            }
         }
     }
 }
